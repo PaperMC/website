b8d435accf2ae2eb95ac716ac6ed58640956c80a
Cubxity <contact@cubxity.dev>
2022-05-21 20:43:52 +0200

    feat: windi configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
+
 import "windi.css";
+import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
 export default MyApp;
