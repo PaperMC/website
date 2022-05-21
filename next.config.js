7c4930791856db8879ae4ac58739beb660f95aac
Cubxity <contact@cubxity.dev>
2022-05-21 21:04:02 +0200

    build: add svg loader

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.plugins.push(new WindiCSSWebpackPlugin());
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: { dimensions: false },
+        },
+      ],
+    });
+
     return config;
   },
 };
