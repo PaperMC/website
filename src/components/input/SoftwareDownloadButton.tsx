b339251b875244071030e6a63d89fc7679963499
Cubxity <cubxity@protonmail.com>
2022-07-31 22:45:56 +0200

    feat(components): improve software download button

--- src/components/input/SoftwareDownloadButton.tsx
+++ src/components/input/SoftwareDownloadButton.tsx
@@ -14,29 +14,47 @@ const SoftwareDownloadButton = () => {
 
   return (
     <Menu as="div" className="relative w-full">
-      <Menu.Button className="rounded-lg px-5 py-3 flex flex-row items-center w-full md:w-100 bg-blue-600 transition-shadow text-white transition-color gap-8 hover:shadow-lg">
-        <div className="w-8 h-8">
-          <DocumentDownloadIcon />
-        </div>
-        <div className="text-left flex-1">
-          {project && builds && latestBuild ? (
-            <>
-              <span className="font-medium text-lg">
-                {project.name} {project.latestVersion}
-              </span>
-              <p className="text-gray-100">
-                {latestBuild && `Build #${latestBuild.build}`}
-              </p>
-            </>
-          ) : (
-            <>
-              <Skeleton className="w-40 mb-2" />
-              <Skeleton className="w-20 h-5" />
-            </>
-          )}
-        </div>
-        <ChevronDownIcon className="w-6 h-6 text-gray-200" />
-      </Menu.Button>
+      <div className="rounded-lg px-5 py-3 flex flex-row w-full md:w-100 bg-blue-600 transition-shadow text-white transition-color gap-4 hover:shadow-lg">
+        <a
+          className="flex flex-row flex-1 items-center gap-8"
+          href={
+            project &&
+            latestBuild &&
+            getVersionBuildDownloadURL(
+              projectId,
+              project.latestVersion,
+              latestBuild.build,
+              latestBuild.downloads["application"].name
+            )
+          }
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="w-8 h-8">
+            <DocumentDownloadIcon />
+          </div>
+          <div className="text-left flex-1 border-r border-gray-300/50">
+            {project && builds && latestBuild ? (
+              <>
+                <span className="font-medium text-lg">
+                  {project.name} {project.latestVersion}
+                </span>
+                <p className="text-gray-100">
+                  {latestBuild && `Build #${latestBuild.build}`}
+                </p>
+              </>
+            ) : (
+              <>
+                <Skeleton className="w-40 mb-2" />
+                <Skeleton className="w-20 h-5" />
+              </>
+            )}
+          </div>
+        </a>
+        <Menu.Button>
+          <ChevronDownIcon className="w-6 h-6 text-gray-200" />
+        </Menu.Button>
+      </div>
       <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
@@ -70,7 +88,7 @@ const SoftwareDownloadButton = () => {
                         <div className="font-medium">
                           {download.name}
                           {name === "application" && (
-                            <span className="ml-2 text-xs rounded-full py-0.5 px-2 bg-yellow-200/80">
+                            <span className="ml-2 text-xs rounded-full py-0.5 px-2 bg-yellow-200/80 text-yellow-800">
                               Recommended
                             </span>
                           )}
