31d183d9a302f45da67cf093c8f0059ac46c93cd
Cubxity <cubxity@protonmail.com>
2022-08-24 09:26:37 +0200

    fix(build): file locations

--- src/lib/service/types.ts
+++ src/lib/service/types.ts
@@ -56,4 +56,3 @@ export interface BuildDownload {
 export interface ProjectsResponse {
   projects: string[];
 }
-
--- src/lib/service/v2.ts
+++ src/lib/service/v2.ts
@@ -1,10 +1,16 @@
 import useSWR, { SWRResponse } from "swr";
-import { Project, ProjectsResponse, VersionBuilds, VersionFamilyBuilds } from "~/service/types";
+
+import {
+  Project,
+  ProjectsResponse,
+  VersionBuilds,
+  VersionFamilyBuilds,
+} from "@/lib/service/types";
 
 const API_ENDPOINT = "https://api.papermc.io/v2";
 
 const fetcher = (path: string) =>
-  fetch(API_ENDPOINT + path).then(res => res.json());
+  fetch(API_ENDPOINT + path).then((res) => res.json());
 
 export const useProjects = (): SWRResponse<ProjectsResponse> =>
   useSWR("/projects", fetcher);
@@ -12,22 +18,34 @@ export const useProjects = (): SWRResponse<ProjectsResponse> =>
 export const useProject = (project: string): SWRResponse<Project> =>
   useSWR(`/projects/${project}`, fetcher);
 
-export const useVersionBuilds = (project: string, version: string): SWRResponse<VersionBuilds> =>
+export const useVersionBuilds = (
+  project: string,
+  version: string
+): SWRResponse<VersionBuilds> =>
   useSWR(`/projects/${project}/versions/${version}/builds`, fetcher);
 
-export const useVersionFamilyBuilds = (project: string, family: string): SWRResponse<VersionFamilyBuilds> =>
+export const useVersionFamilyBuilds = (
+  project: string,
+  family: string
+): SWRResponse<VersionFamilyBuilds> =>
   useSWR(`/projects/${project}/version_group/${family}/builds`, fetcher);
 
 // TODO: Better error handling?
-const getJSON = <T>(path: string): Promise<T> =>
-  fetcher(path);
+const getJSON = <T>(path: string): Promise<T> => fetcher(path);
 
 export const getProject = (project: string): Promise<Project> =>
   getJSON(`/projects/${project}`);
 
-export const getVersionBuilds = (project: string, version: string): Promise<VersionBuilds> =>
+export const getVersionBuilds = (
+  project: string,
+  version: string
+): Promise<VersionBuilds> =>
   getJSON(`/projects/${project}/versions/${version}/builds`);
 
-export const getVersionBuildDownloadURL = (project: string, version: string, build: number, file: string): string =>
+export const getVersionBuildDownloadURL = (
+  project: string,
+  version: string,
+  build: number,
+  file: string
+): string =>
   `${API_ENDPOINT}/projects/${project}/versions/${version}/builds/${build}/downloads/${file}`;
-
