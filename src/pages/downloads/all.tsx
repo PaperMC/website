f21aa95c90c88c709593c84a3519b51c6c1d9ec6
Madeline Miller <mnmiller1@me.com>
2022-10-15 14:49:33 +1000

    chore: re-add link to article about old MC versions

--- src/pages/downloads/all.tsx
+++ src/pages/downloads/all.tsx
@@ -43,7 +43,9 @@ const LegacyDownloads: NextPage<LegacyDownloadProps> = ({
       <div className="flex flex-col h-screen">
         <div className="h-14" />
         <div className="text-center px-4 py-2 font-bold bg-red-400 dark:bg-red-500 shadow-md">
-          Legacy builds are not supported. Proceed at your own risk!
+          <a href="https://madelinemiller.dev/blog/which-minecraft-version/">
+            Legacy builds are not supported. Proceed at your own risk!
+          </a>
         </div>
         <div className="flex-1 flex flex-row min-h-0">
           <DownloadsTree
