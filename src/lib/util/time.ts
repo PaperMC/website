74d535f8a37be608b2bb2bb2ac39ed664f45c8f5
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2022-11-04 19:07:17 +0100

    fix: format logger iso time

--- src/lib/util/time.ts
+++ src/lib/util/time.ts
@@ -36,3 +36,10 @@ export const formatISODateTime = (date: Date): string => {
   const minute = String(date.getMinutes()).padStart(2, "0");
   return `${date.getFullYear()}-${month}-${day} ${hour}:${minute}`;
 };
+
+export const formatISOFullTime = (date: Date): string => {
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+  return `${hour}:${minute}:${second}`;
+};
