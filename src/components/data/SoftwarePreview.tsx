a5028f23538c7f92c412cb6ea1829d6436000b0f
Cubxity <cubxity@protonmail.com>
2022-12-24 21:35:37 +0100

    feat(components): improve software preview cards

--- src/components/data/SoftwarePreview.tsx
+++ src/components/data/SoftwarePreview.tsx
@@ -19,7 +19,7 @@ const SoftwarePreview = ({
 }: SoftwarePreviewProps) => (
   <Link href={download ? `/downloads/${id}` : `/software/${id}`} passHref>
     <a>
-      <article className="rounded-xl transition-shadow transition-color p-4 md:p-8 hover:(shadow-lg bg-primary-300 dark:bg-gray-800)">
+      <article className="rounded-xl transition-all h-full p-4 md:p-8 hover:(shadow-lg bg-primary-300 dark:bg-gray-800)">
         <div className="flex flex-row items-center gap-4 mb-4">
           <div className="rounded-lg w-12 h-12 bg-gray-800 p-3">
             <Icon />
