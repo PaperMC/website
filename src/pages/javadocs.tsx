181177d9d326c704ec6135c1867ec1649afac17e
MiniDigger | Martin <admin@benndorf.dev>
2023-03-20 18:26:58 +0100

    feat: add javadocs page (#44)
    * feat: add javadocs page
    
    * now we less network requests

---
+++ src/pages/javadocs.tsx
@@ -0,0 +1,74 @@
+import type { NextPage, GetStaticProps } from "next";
+
+import PaperIcon from "@/assets/brand/paper.svg";
+import VelocityIcon from "@/assets/brand/velocity.svg";
+import WaterfallIcon from "@/assets/brand/waterfall.svg";
+import SoftwarePreview from "@/components/data/SoftwarePreview";
+import SEO from "@/components/util/SEO";
+import { getProject } from "@/lib/service/v2";
+
+interface JavadocProps {
+  paperVersion: string;
+  velocityVersion: string;
+  waterfallVersion: string;
+}
+
+const Javadocs: NextPage<JavadocProps> = ({
+  paperVersion,
+  velocityVersion,
+  waterfallVersion,
+}: JavadocProps) => {
+  return (
+    <>
+      <SEO
+        title="Javadocs"
+        description="Find javadocs for our software – including Paper, Velocity, and Waterfall."
+        keywords={["papermc", "paper", "javadocs", "velocity", "waterfall"]}
+      />
+      <header className="max-w-7xl flex flex-col items-center mx-auto px-4 pt-32 pb-16 lg:(pt-48 pb-26) gap-2">
+        <h1 className="font-medium leading-normal lg:(text-5xl leading-normal) text-4xl">
+          Javadocs
+        </h1>
+        <p className="text-xl text-center mb-6">
+          {"You can find the javadocs for our software below:"}
+        </p>
+        <div className="grid md:grid-cols-3 lg:mt-6 gap-2 px-2 xl:gap-4">
+          <SoftwarePreview
+            id="paper"
+            name="Paper"
+            icon={PaperIcon}
+            javadocs={paperVersion}
+          />
+          <SoftwarePreview
+            id="velocity"
+            name="Velocity"
+            icon={VelocityIcon}
+            javadocs={velocityVersion}
+          />
+          <SoftwarePreview
+            id="waterfall"
+            name="Waterfall"
+            icon={WaterfallIcon}
+            javadocs={waterfallVersion}
+          />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Javadocs;
+
+export const getStaticProps: GetStaticProps<JavadocProps> = async () => {
+  const { version_groups: paper_groups } = await getProject("paper");
+  const { version_groups: velocity_groups } = await getProject("velocity");
+  const { version_groups: waterfall_groups } = await getProject("waterfall");
+  return {
+    props: {
+      paperVersion: paper_groups[paper_groups.length - 1],
+      velocityVersion: velocity_groups[velocity_groups.length - 1],
+      waterfallVersion: waterfall_groups[waterfall_groups.length - 1],
+    },
+    revalidate: 3600, // 1 hour
+  };
+};
