26279dbea8dcb5b06a0209c00ba5d64753fcbda7
Simeon L <78266888+DerSimeon@users.noreply.github.com>
2023-03-21 18:46:18 +0100

    fix(components): copy software hash button (#42)
    * remove unncesseary button to reduce complexity
    
    * moved svg to its own file
    
    * increase margin to .5rem and change color to match sha256 text
    
    * remove curly brackets
    
    * cancel previous timeout when copying multiple times
    
    * reverted the removal of clipboard
    
    * increased hitbox for the copy button

--- src/components/input/SoftwareDownloadButton.tsx
+++ src/components/input/SoftwareDownloadButton.tsx
@@ -1,6 +1,7 @@
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useContext, useState } from "react";
 
+import CloneIcon from "@/assets/icons/fontawesome/clone-icon.svg";
 import ChevronDownIcon from "@/assets/icons/heroicons/chevron-down.svg";
 import DocumentDownloadIcon from "@/assets/icons/heroicons/document-download.svg";
 import Skeleton from "@/components/data/Skeleton";
@@ -12,6 +13,17 @@ const SoftwareDownloadButton = () => {
 
   const latestBuild = builds && builds[builds.length - 1];
   const [copied, setCopied] = useState("");
+  const [timeoutHandler, setTimeoutHandler] = useState<NodeJS.Timeout | null>(
+    null
+  );
+
+  const updateCopied = (text: string) => {
+    if (timeoutHandler) {
+      clearTimeout(timeoutHandler);
+    }
+    setCopied(text);
+    setTimeoutHandler(setTimeout(() => setCopied(""), 2000));
+  };
 
   return (
     <Menu as="div" className="relative w-full">
@@ -99,31 +111,17 @@ const SoftwareDownloadButton = () => {
                             </span>
                           )}
                         </div>
-                        <div className="text-gray-700 dark:text-gray-300 text-xs mt-1 truncate">
+                        <div className="text-gray-700 dark:text-gray-300 text-xs">
                           {download.sha256}
-                          <span className={"ml-1"}>
-                            <button
-                              className="bg-gray-200 dark:bg-gray-800"
-                              onClick={(evt) => {
-                                evt.preventDefault();
-                                navigator.clipboard.writeText(download.sha256);
-                                setCopied(download.sha256);
-                                setTimeout(() => setCopied(""), 2000);
-                              }}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-4 w-4"
-                                viewBox="0 0 512 512"
-                                fill="#fefefe"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z"
-                                  clipRule="evenodd"
-                                />
-                              </svg>
-                            </button>
+                          <span
+                            className="ml-2 h-6 w-6 inline-flex items-center justify-center"
+                            onClick={(evt) => {
+                              evt.preventDefault();
+                              navigator.clipboard.writeText(download.sha256);
+                              updateCopied(download.sha256);
+                            }}
+                          >
+                            <CloneIcon className="h-4 w-4 text-gray-700 dark:text-gray-300" />
                           </span>
                         </div>
                       </div>
