2eecb3669f461410cfe8c963fa7480dd7d83d98e
powercas_gamer <cas@mizule.dev>
2023-10-09 08:56:44 +0200

    add documentation link to navbar (#78)
    * add documentation link to navbar
    
    * chore: lint
    
    ---------
    
    Co-authored-by: MiniDigger | Martin <admin@benndorf.dev>

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -98,6 +98,14 @@ const NavBar = ({ component }: NavBarProps) => {
             <ExternalUrlIcon className="h-4 w-4 ml-1 align-sub" />
           </NavLink>
           <NavLink
+            href="https://docs.papermc.io/"
+            target="_blank"
+            className="inline-flex items-center"
+          >
+            Docs
+            <ExternalUrlIcon className="h-4 w-4 ml-1 align-sub" />
+          </NavLink>
+          <NavLink
             href="https://forums.papermc.io/"
             target="_blank"
             className="inline-flex items-center"
