d8d75157cf40ef217521d5a347cc4c1c7be8c86c
Chetan-KK <chetankhulage350@gmail.com>
2023-10-11 17:45:21 +0530

    404 page bg problem fixed

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      404 | page not found
+    </div>
+  );
+};
+
+export default notFound;
