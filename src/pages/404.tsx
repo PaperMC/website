6d58d4a3409e13a1f826a614354f76d84180790e
Chetan-KK <chetankhulage350@gmail.com>
2023-10-13 20:19:33 +0530

    404 updated

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,10 @@
+import ErrorIllustration from "@/assets/illustrations/undraw/404.svg";
+
 const notFound = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      404 | page not found
+      <ErrorIllustration className="w-96 " />
+      <p>404 page not found</p>
     </div>
   );
 };
