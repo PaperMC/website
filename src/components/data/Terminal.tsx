50176c13f00ecb20e56e0a281d21196554b678dc
Tamion <70228790+notTamion@users.noreply.github.com>
2024-05-02 21:50:18 +0200

    unknown command bloat

--- src/components/data/Terminal.tsx
+++ src/components/data/Terminal.tsx
@@ -73,7 +73,7 @@ export function Terminal({ project }: ProjectProps) {
           break;
         }
         default: {
-          currentCmdOutput = "Unknown Command";
+          currentCmdOutput = "Unknown command. Type \"help\" for help.";
         }
       }
       setCmdOutput([
