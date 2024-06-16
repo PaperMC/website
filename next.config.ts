fb6854f434d256da92b26218310a426fb4f711e4
powercas_gamer <cas@mizule.dev>
2024-06-16 13:23:41 +0000

    add commit footer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+import childProcess from "child_process";
+
 import type { NextConfig } from "next";
+
+let currentCommit;
+try {
+  currentCommit = childProcess.execSync("git rev-parse HEAD").toString().slice(0, 7).trim();
+} catch (error) {
+  console.error("Failed to get the current commit:", error);
+  currentCommit = "unknown";
+}
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  env: {
+    CURRENT_COMMIT: currentCommit,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
