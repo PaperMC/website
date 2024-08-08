e2ce81eb86cf34b0fedb9b49aeba091aaf3e34f7
Glare <admin@glaremasters.me>
2024-08-08 16:21:40 -0500

    feat: add in cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,24 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon({
-      iconDir: "src/assets",
-    }),
-    UnoCSS({
-      injectReset: true,
-    }),
-    react(),
-  ],
+  integrations: [icon({
+    iconDir: "src/assets"
+  }), UnoCSS({
+    injectReset: true
+  }), react()],
   image: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.opencollective.com",
-      },
-    ],
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "**.githubusercontent.com"
+    }, {
+      protocol: "https",
+      hostname: "**.opencollective.com"
+    }]
   },
+  output: "hybrid",
+  adapter: cloudflare()
 });
