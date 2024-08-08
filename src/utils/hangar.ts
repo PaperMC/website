1eac824ce73fd89d665535b5c7374ee31b6f8c17
Glare <admin@glaremasters.me>
2024-08-08 17:01:14 -0500

    feat: javadoc page + javadoc link on project pages

---
+++ src/utils/hangar.ts
@@ -0,0 +1,8 @@
+import type { HangarProjectList } from "@/types/hangar";
+
+export const getHangarProjects = (
+    platform: string,
+): Promise<HangarProjectList> =>
+    fetch(
+        `https://hangar.papermc.io/api/v1/projects?limit=1&offset=0&sort=-stars&platform=${platform.toUpperCase()}`,
+    ).then((res) => res.json());
