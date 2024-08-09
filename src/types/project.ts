61a9460798d77bb76699d7aa691d969bb2213ffc
Riley Park <rileysebastianpark@gmail.com>
2024-08-08 17:00:07 -0700

    chore: format

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,58 +1,58 @@
 export interface Project {
-    project_id: string;
-    project_name: string;
-    version_groups: string[];
-    versions: string[];
+  project_id: string;
+  project_name: string;
+  version_groups: string[];
+  versions: string[];
 }
 
 export interface ProjectVersion {
-    project_id: string;
-    project_name: string;
-    version: string;
-    builds: number[];
+  project_id: string;
+  project_name: string;
+  version: string;
+  builds: number[];
 }
 
 export interface VersionBuilds {
-    project_id: string;
-    project_name: string;
-    version: string;
-    builds: Build[];
+  project_id: string;
+  project_name: string;
+  version: string;
+  builds: Build[];
 }
 
 export interface VersionFamilyBuilds {
-    project_id: string;
-    project_name: string;
-    version_group: string;
-    versions: string[];
-    builds: VersionFamilyBuild[];
+  project_id: string;
+  project_name: string;
+  version_group: string;
+  versions: string[];
+  builds: VersionFamilyBuild[];
 }
 
 export interface VersionFamilyBuild extends Build {
-    project_id: string;
-    project_name: string;
-    version: string;
+  project_id: string;
+  project_name: string;
+  version: string;
 }
 
 export interface Build {
-    build: number;
-    time: string;
-    channel: "default" | "experimental";
-    promoted: boolean;
-    changes: BuildChange[];
-    downloads: Record<string, BuildDownload>;
+  build: number;
+  time: string;
+  channel: "default" | "experimental";
+  promoted: boolean;
+  changes: BuildChange[];
+  downloads: Record<string, BuildDownload>;
 }
 
 export interface BuildChange {
-    commit: string;
-    summary: string;
-    message: string;
+  commit: string;
+  summary: string;
+  message: string;
 }
 
 export interface BuildDownload {
-    name: string;
-    sha256: string;
+  name: string;
+  sha256: string;
 }
 
 export interface ProjectsResponse {
-    projects: string[];
+  projects: string[];
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,26 +1,46 @@
-import type { Project, ProjectsResponse, VersionBuilds, VersionFamilyBuilds } from "@/types/project";
+import type {
+  Project,
+  ProjectsResponse,
+  VersionBuilds,
+  VersionFamilyBuilds,
+} from "@/types/project";
 
 const API_ENDPOINT = "https://api.papermc.io/v2";
 
 export async function fetchProjects(): Promise<ProjectsResponse> {
-    const response = await fetch(`${API_ENDPOINT}/projects`);
-    return response.json();
+  const response = await fetch(`${API_ENDPOINT}/projects`);
+  return response.json();
 }
 
 export async function fetchProject(project: string): Promise<Project> {
-    const response = await fetch(`${API_ENDPOINT}/projects/${project}`);
-    return response.json();
+  const response = await fetch(`${API_ENDPOINT}/projects/${project}`);
+  return response.json();
 }
 
-export async function fetchVersionBuilds(project: string, version: string): Promise<VersionBuilds> {
-    const response = await fetch(`${API_ENDPOINT}/projects/${project}/versions/${version}/builds`);
-    return response.json();
+export async function fetchVersionBuilds(
+  project: string,
+  version: string,
+): Promise<VersionBuilds> {
+  const response = await fetch(
+    `${API_ENDPOINT}/projects/${project}/versions/${version}/builds`,
+  );
+  return response.json();
 }
 
-export async function fetchVersionFamilyBuilds(project: string, family: string): Promise<VersionFamilyBuilds> {
-    const response = await fetch(`${API_ENDPOINT}/projects/${project}/version_group/${family}/builds`);
-    return response.json();
+export async function fetchVersionFamilyBuilds(
+  project: string,
+  family: string,
+): Promise<VersionFamilyBuilds> {
+  const response = await fetch(
+    `${API_ENDPOINT}/projects/${project}/version_group/${family}/builds`,
+  );
+  return response.json();
 }
 
-export const generateVersionBuildDownloadURL = (project: string, version: string, build: number, file: string): string =>
-    `${API_ENDPOINT}/projects/${project}/versions/${version}/builds/${build}/downloads/${file}`;
+export const generateVersionBuildDownloadURL = (
+  project: string,
+  version: string,
+  build: number,
+  file: string,
+): string =>
+  `${API_ENDPOINT}/projects/${project}/versions/${version}/builds/${build}/downloads/${file}`;
