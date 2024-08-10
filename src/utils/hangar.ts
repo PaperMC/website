3da21a0891d7eb4f771b003a4b754afdad3ff8b4
Glare <admin@glaremasters.me>
2024-08-10 11:54:43 -0500

    feat: add in hangar project counts on software pages

--- src/utils/hangar.ts
+++ src/utils/hangar.ts
@@ -1,9 +1,15 @@
 import type { HangarProjectList } from "@/types/hangar";
 
-export const getHangarProjects = (
+export const fetchHangarProjects = async (
   platform: string,
 ): Promise<HangarProjectList> => {
-  return fetch(
-    `https://hangar.papermc.io/api/v1/projects?limit=1&offset=0&sort=-stars&platform=${platform.toUpperCase()}`,
-  ).then((res) => res.json());
+  const res = await fetch(
+    `https://hangar.papermc.io/api/v1/projects?limit=1&offset=0&sort=-stars&platform=${platform.toUpperCase()}`);
+  return await res.json();
 };
+
+export const fetchHangarFoliaProjects = async () :Promise<HangarProjectList> => {
+  const res = await fetch(
+    "https://hangar.papermc.io/api/v1/projects?limit=10&offset=0&tag=SUPPORTS_FOLIA&sort=-stars");
+  return await res.json();
+}
