2cc1b88c819a708bb9c50a9dad000c23ceddf2bc
powercas_gamer <cas@mizule.dev>
2024-10-21 16:45:48 +0000

    footer git commit fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
 import cloudflare from "@astrojs/cloudflare";
+import { execSync } from "node:child_process";
+
+process.env.GIT_COMMIT_HASH = (process.env.GITHUB_SHA || "").trim().substring(0, 7) || fetchGitCommitHash();
+
+function fetchGitCommitHash() {
+  return execSync("git rev-parse --short HEAD").toString().trim();
+}
 
 // https://astro.build/config
 export default defineConfig({
