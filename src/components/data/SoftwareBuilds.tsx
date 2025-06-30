d8004ae71a42dee081aa0b81d5a790380b977f7a
mja00 <teamnirvana2@gmail.com>
2025-06-30 17:52:41 -0400

    feat: add divider between builds

--- src/components/data/SoftwareBuilds.tsx
+++ src/components/data/SoftwareBuilds.tsx
@@ -16,32 +16,34 @@ export interface SoftwareBuildsProps {
 const SoftwareBuilds = ({ project, version, builds, eol }: SoftwareBuildsProps) => (
   <div className="flex flex-col gap-1">
     {builds &&
-      builds.slice(0, 10).map((build) => (
-        <div
-          className="flex flex-row items-center hover:bg-blue-100 dark:hover:bg-gray-900 px-4 py-2 rounded-lg transition-colors"
-          key={build.id}
-        >
-          {/* eslint-disable-next-line react/jsx-no-target-blank */}
-          <a
-            role="button"
-            href={build.downloads["server:default"].url}
-            target="_blank"
-            className={clsx(
-              "text-gray-100 text-sm text-center font-medium rounded-full p-2 min-w-16 mr-4 inline-flex items-center gap-1",
-              build.channel === "STABLE" && !eol ? "bg-gray-800" : "bg-red-500",
-            )}
-          >
-            <DownloadIcon className="w-4 h-4" />#{build.id}
-          </a>
-          <div className="flex-1 flex flex-col text-gray-900 dark:text-gray-200 min-w-0">
-            <SoftwareBuildChanges project={project} build={build} version={version} />
+      builds.slice(0, 10).map((build, idx) => (
+        <div key={build.id}>
+          <div className="flex flex-row items-start hover:bg-blue-100 dark:hover:bg-gray-900 px-4 py-2 rounded-lg transition-colors">
+            {/* eslint-disable-next-line react/jsx-no-target-blank */}
+            <a
+              role="button"
+              href={build.downloads["server:default"].url}
+              target="_blank"
+              className={clsx(
+                "text-gray-100 text-sm text-center font-medium rounded-full p-2 min-w-16 mr-4 inline-flex items-center gap-1 mt-1",
+                build.channel === "STABLE" && !eol ? "bg-gray-800" : "bg-red-500",
+              )}
+            >
+              <DownloadIcon className="w-4 h-4" />#{build.id}
+            </a>
+            <div className="flex-1 flex flex-col text-gray-900 dark:text-gray-200 min-w-0">
+              <SoftwareBuildChanges project={project} build={build} version={version} />
+            </div>
+            <div
+              className="hidden md:block text-gray-500 dark:text-gray-300 mt-1 ml-2"
+              title={formatISODateTime(new Date(build.time))}
+            >
+              {formatRelativeDate(new Date(build.time))}
+            </div>
           </div>
-          <div
-            className="hidden md:block text-gray-500 dark:text-gray-300 mt-1 ml-2"
-            title={formatISODateTime(new Date(build.time))}
-          >
-            {formatRelativeDate(new Date(build.time))}
-          </div>
+          {idx < builds.slice(0, 10).length - 1 && (
+            <hr className="border-t border-gray-300 dark:border-gray-700 mx-4" />
+          )}
         </div>
       ))}
     {!builds &&
