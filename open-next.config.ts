6626de13c1e319f5613a557893495ffe9bf02773
darbyjack <admin@glaremasters.me>
2025-10-06 21:32:53 -0500

    feat: isr

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/dist/api/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
+    incrementalCache: r2IncrementalCache,
+    enableCacheInterception: true,
 });
