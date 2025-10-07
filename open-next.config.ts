31c34b56c6ead831c2c3c2da0feaafce7458d421
Riley Park <rileysebastianpark@gmail.com>
2025-10-06 22:56:30 -0700

    chore: remove regional cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
-import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
-    mode: "long-lived",
-    shouldLazilyUpdateOnCacheHit: true,
-  }),
+  incrementalCache: r2IncrementalCache,
   enableCacheInterception: true,
-  queue: doQueue,
 });
