c4f7d49351abf8a3794b1103d61079d864a3c49b
Riley Park <rileysebastianpark@gmail.com>
2025-10-06 23:53:26 -0700

    chore: try using a different DO

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,9 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
   enableCacheInterception: true,
+  incrementalCache: r2IncrementalCache,
+  queue: doQueue,
 });
