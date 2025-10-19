a87aed187f281925598e1f126ba57666b308a4ff
darbyjack <admin@glaremasters.me>
2025-10-19 13:32:56 -0500

    chore: adjust post location + remove broken links for now

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const posts = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./content/posts" }),
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/posts" }),
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
