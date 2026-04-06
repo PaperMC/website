c28fcde83eeadae5da842b6fd25b4336d5bcab16
Strokkur24 <strokkur.24@gmail.com>
2026-04-06 17:35:22 +0200

    Fix lint issues

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,9 @@
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
-  interface Locals extends Runtime {}
+  interface Locals extends Runtime {
+    runtime?: {
+      env: Env;
+    };
+  }
 }
