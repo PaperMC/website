3b40ae1870b12a33d3513d12398bbf8eabcff808
darbyjack <admin@glaremasters.me>
2026-07-01 06:35:36 -0500

    fix(rss): ensure posts are sorted by date

--- src/pages/news/rss.xml.ts
+++ src/pages/news/rss.xml.ts
@@ -2,20 +2,24 @@ import type { APIRoute } from "astro";
 import rss from "@astrojs/rss";
 import { getCollection, type CollectionEntry } from "astro:content";
 
+const getPostDate = (post: CollectionEntry<"posts">) => post.data.pubDate ?? post.data.date;
+
 export const GET: APIRoute = async ({ site }) => {
   const posts = await getCollection("posts");
 
-  const items = posts.map((post: CollectionEntry<"posts">) => {
-    const data = post.data;
-    const pubDate = data.pubDate ?? data.date ?? undefined;
+  const items = posts
+    .sort((a, b) => getPostDate(b).valueOf() - getPostDate(a).valueOf())
+    .map((post) => {
+      const data = post.data;
+      const pubDate = getPostDate(post);
 
-    return {
-      title: data.title,
-      pubDate,
-      link: `/news/${post.id}`,
-      author: data.author,
-    };
-  });
+      return {
+        title: data.title,
+        pubDate,
+        link: `/news/${post.id}`,
+        author: data.author,
+      };
+    });
 
   return rss({
     title: "PaperMC News",
